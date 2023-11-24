3abf1ac2be583c496d58497bd95e20124ea27395
edisonoda <edison.oda@hotmail.com>
2023-11-24 03:40:58 -0300

    Add random creation of Tyrant and Springs in Factory

--- include/Stage/Factory.h
+++ include/Stage/Factory.h
@@ -3,6 +3,8 @@
 
 /* Program Defined */
 #include "Stage/Stage.h"
+#include "Entities/Characters/Tyrant.h"
+#include "Entities/Obstacles/Spring.h"
 
 /* Forward Declaration */
 namespace States { class GameState; }
@@ -10,7 +12,19 @@ namespace States { class GameState; }
 namespace Stages {
   class Factory : public Stage {
     private:
+      Entities::Characters::Tyrant* m_pTyrant;
+      Entities::Obstacles::Spring* m_pSpring;
+      const int m_maxTyrantInstances;
+      const int m_maxSpringInstances;
+      const int m_tyrantChance;
+      const int m_springChance;
+      int m_tyrantNumber;
+      int m_springNumber;
+
+    private:
       virtual void loadTextures();
+      virtual void createTyrant(sf::Vector2f& position);
+      virtual void createSpring(sf::Vector2f& position);
 
     public:
       Factory(
--- src/Stage/Factory.cpp
+++ src/Stage/Factory.cpp
@@ -8,6 +8,14 @@
 namespace Stages {
   Factory::Factory(ResourceHolder<Textures::ID, sf::Texture>* pTextureHolder, Entities::EntityFactory* pEntityFactory, States::GameState* pGameState) 
     : Stage(pTextureHolder, pEntityFactory, pGameState, Constants::FACTORY)
+    , m_pTyrant(NULL)
+    , m_maxTyrantInstances(2)
+    , m_tyrantChance(20)
+    , m_tyrantNumber(0)
+    , m_pSpring(NULL)
+    , m_maxSpringInstances(4)
+    , m_springChance(40)
+    , m_springNumber(0)
   {
     m_bgMusic.openFromFile(Sounds::FACTORY_BG);
     m_bgMusic.setVolume(10);
@@ -49,17 +57,43 @@ namespace Stages {
     m_pTextureHolder->load(Textures::MovingPlatform, Textures::MOVING_PLATFORM);
   }
 
-  void Factory::createRandomEnemy(sf::Vector2f& position) {
-    sf::Vector2f size = m_pGraphicsManager->getStageSize();
+  void Factory::createTyrant(sf::Vector2f& position) {
+    if (m_tyrantNumber >= m_maxTyrantInstances)
+      return;
 
-    if (position.y > size.y / 2) {
-      createGoomba(position);
-    } else {
-      // Spawn Tyrant
+    const int random = 1 + rand() % 100;
+    if (random < m_tyrantChance) {
+      sf::Texture& textureRef = m_pEntityFactory->getTexture(Textures::Tyrant);
+      m_pTyrant = new Entities::Characters::Tyrant(Textures::Tyrant, textureRef, position, static_cast<Stage*>(this));
+      m_dynamicEntities.include(static_cast<Entities::Entity*>(m_pTyrant));
     }
   }
 
+  void Factory::createSpring(sf::Vector2f& position) {
+    if (m_springNumber >= m_maxSpringInstances)
+      return;
+
+    const int random = 1 + rand() % 100;
+    if (random < m_springChance) {
+      sf::Texture& textureRef = m_pEntityFactory->getTexture(Textures::Spring);
+      m_pSpring = new Entities::Obstacles::Spring(Textures::Spring, textureRef, position);
+      m_dynamicEntities.include(static_cast<Entities::Entity*>(m_pSpring));
+    }
+  }
+
+  void Factory::createRandomEnemy(sf::Vector2f& position) {
+    sf::Vector2f size = m_pGraphicsManager->getStageSize();
+
+    if (position.y > size.y / 2)
+      createGoomba(position);
+    else
+      createTyrant(position);
+  }
+
   void Factory::createRandomObstacle(sf::Vector2f& position) {
-    
+    if (rand() % 2)
+      createSpike(position);
+    else
+      createSpring(position);
   }
 }
